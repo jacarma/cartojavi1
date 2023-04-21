707e88bde4b087d65d8f0199045914c983036182
Javi Carrasco <jacarma@gmail.com>
2023-04-21 09:52:25 +0200

    fix: remove duplicate app folder

--- src/app/layer/initCarto.ts
+++
@@ -1,8 +0,0 @@
-import { setDefaultCredentials } from '@deck.gl/carto/typed';
-
-export function initCarto() {
-  setDefaultCredentials({
-    accessToken:
-      'eyJhbGciOiJIUzI1NiJ9.eyJhIjoiYWNfbGU5emg0MGUiLCJqdGkiOiIzNDc0ZmM5NiJ9.PBktjbtXa9po2aN_NWWyYGL5HZkAJQiKL93j_bqZXo8',
-  });
-}
